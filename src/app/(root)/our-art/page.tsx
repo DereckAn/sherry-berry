6186c0c6489084468c1f42e08e4f6e82d1585823
DereckAn <dereckangeles15071998@gmail.com>
2025-11-12 15:29:41 -0700

    Creating Art page

---
+++ src/app/(root)/our-art/page.tsx
@@ -0,0 +1,11 @@
+import { main } from "framer-motion/client";
+
+const OurArtPage = () => {
+    return ( 
+        <main>
+            <h1>Here you can see all the process of creating the products </h1>
+        </main>
+     );
+}
+ 
+export default OurArtPage;
